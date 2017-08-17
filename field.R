b7d29b1a3d70dee4993c671d8a0ee8a9225d306f
Jingyi Xiao <30737379+JingyiXiaoXiao@users.noreply.github.com>
2017-08-17 15:51:03 -0700

    Update field.R

--- field.R
+++ field.R
@@ -4,14 +4,12 @@
 # 08/2017
 
 
-# Robert J. Hijmans (2016). raster: Geographic Data Analysis and Modeling. R
-# package version 2.5-8. https://CRAN.R-project.org/package=raster
 library(raster)
 
 # constructor of field
 # x could be image, matrix
 # domain and granularity are optional
-field <- function(x, domain = NULL, sn = NULL, granularity == NULL){
+field <- function(x, domain = NULL, sn = NULL, granularity = NULL){
   if(is.null(x))
     stop("Input is needed for field data.")
   else 
@@ -37,8 +35,6 @@ valueAt <- function(field, qm = "index", var1, var2 = NULL){
     # var1, var2 should be in the range of [0,nrow], [0, ncol], 
     # which is checked in raster package
     return(field[var1,var2])
-  # else if(qm == "latlon")
-  #   return(field[var1,var2])
   else stop("Invalid location.")
 }
 
@@ -50,13 +46,13 @@ setValue <- function(field, qm = "index", var1, var2 = NULL, value){
     stop("Invalid value for this location.")
   else if(qm == "index" & (var1%%1 == 0)){
     field[var1] <- value
-    return("Set value successfully!")
+    return(field)
   }
   else if(qm == "rowcol" & (var1%%1 == 0) & (var2%%1 == 0)){
     # var1, var2 should be in the range of [0,nrow], [0, ncol], 
     # which is checked in raster package
     field[var1,var2] <- value
-    return("Set value successfully!")
+    return(field)
   }
   # else if(qm == "latlon")
   #   return(field[var1,var2])
@@ -64,6 +60,7 @@ setValue <- function(field, qm = "index", var1, var2 = NULL, value){
 }
 
 # get a field's domain
+# what kind of value should reture
 # for now
 domain <- function(field){
   return (rasterToPolygons(field))
@@ -71,22 +68,21 @@ domain <- function(field){
 
 # restrict the domain
 # domain could be raster or features
-setDomain <- function(field, domain, sn){
-  if(is.null(field))
+setDomain <- function(field, domain, sn = "in"){
+  if(class(field) != "RasterLayer")
     stop("Invalid field data")
-  else if(is.null(sn)|sn == "in"){
-    if(class(domain)=="RasterLayer"|class(domain) == "SpatialPolygonsDataFrame"){
+  else{
+    if(sn == "in"){
       fld <- crop(field,extent(domain),snap=sn)
       fld <- mask(x=fld , mask = domain)
     }
     else if(sn == "out"){
+      # !!!!! extent needs to be the same. figure out
       fld <- mask(x=field, mask = domain, inverse = TRUE)
     }
-      stop("Invalid domain data")
+    else 
+      stop("Invalid domain data.")
   }
-  # else if(sn == "out"){
-  #   fld <- mask(field, mask = domain, )
-  # }
   return (fld) 
 }
 
@@ -130,22 +126,31 @@ getGranularity <- function(field){
 
 # set granularity for fields 
 # could be finer or corse granularity
-setGranularity <- function(field, xres, yres = NULL){
+setGranularity <- function(field, x, y = NULL, func = mean){
   if(class(field) == "RasterLayer"){
-    if(is.null(yres)){
-      if(xres >= xres(field))
-        return (aggregate(field, fact = xres/xres(field)))
+    xr <- xres(field)
+   yr <- yres(field)
+    if(is.null(y)){
+      if(x >= xr)
+        n <- aggregate(field, fact = x/xr, fun = func)
       else
-        return(disaggregate(field, fact = xres(field)/xres))
+        n <- disaggregate(field, fact = xr/x, fun = func)
     }
     else{
-      if(xres >= xres(field) && yres >= yres(field))
-        return (aggregate(field, fact = (xres/xres(field)),yres/yres(field)))
-      else if((xres <= xres(field) && yres <= yres(field)))
-        return(disaggregate(field, fact = (xres(field)/xres,yres(field)/yres)))
+      if(x >= xr && y >= y)
+        n <- aggregate(field, fact = c(x/xr,y/yr), fun = func)
+      else if(x <= xr && y <= yr)
+        n <- disaggregate(field, fact = c(xr/x, yr/y), fun = func)
+      else
+        stop("Invalid granularity.")
     }
-    else
-      stop("Invalid granularity.")
-  }
-  else stop("Invalid field data.")
+ return(n)
+ }
+ else stop("Invalid field data.")
 }
+
+
+
+# more operations
+# field to object
+
